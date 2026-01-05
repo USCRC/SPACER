68832c359180ee6c201818f8845d348d6d89e767
USCRC <sodiq.agoro@us.abb.com>
2026-01-05 14:16:10 -0500

    Delete SixPhaseDrive_New_WithPFC directory

--- SixPhaseDrive_New_WithPFC/Debug/Core/Src/PFC_control.d
+++
@@ -1,3 +0,0 @@
-Core/Src/PFC_control.o: ../Core/Src/PFC_control.c \
- ../Core/Inc/PFC_control.h
-../Core/Inc/PFC_control.h:
--- SixPhaseDrive_New_WithPFC/Debug/Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.d
+++
@@ -1,2 +0,0 @@
-Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.o: \
- C:/Users/USSOAGO/STM32Cube/Repository/STM32Cube_FW_G4_V1.6.1/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_adc.c
